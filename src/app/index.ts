db4d807585770ea4b93a0ee9debebfc9d5bb9b01
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2017-03-10 15:28:25 +0100

    use new system features

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,25 +1,3 @@
-import { App, Server } from 'corpjs-express'
-import Config, { loaders, processors } from 'corpjs-config'
-import Endpoints from 'corpjs-endpoints'
-import Router from './Router'
-import System from 'corpjs-system'
-import Logger from 'corpjs-logger'
-const {name} = require('../../package.json')
+import system from './system'
 
-const inDevelopment = process.env.NODE_ENV === 'dev'
-process.on('unhandledRejection', err => console.error(err))
-
-export default new System({ exitOnError: !inDevelopment })
-    .add('config', new Config()
-        .add(config => loaders.require({ path: './config/default.js', mandatory: true })))
-    .add('logger', Logger()).dependsOn({ component: 'config', source: 'logger', as: 'config' })
-    .add('endpoints', Endpoints()).dependsOn({ component: 'config', source: 'endpoints', as: 'config' })
-    .add('app', App())
-    .add('router', Router()).dependsOn('app', 'endpoints', 'logger')
-    .add('server', Server()).dependsOn('app', 'router', { component: 'config', source: 'server', as: 'config' })
-    .on('componentStart', (componentName: string) => console.log(`Started component: ${componentName}`))
-    .on('componentStop', (componentName: string) => console.log(`Stopped component: ${componentName}`))
-    .on('start', (resources) => {
-        resources.logger.verbose(`Started service: ${name}`)
-    })
-    .on('stop', (err, stopErr) => console.log(`Stopped service: ${name}`, err || '', stopErr || ''))
+system.start()
---
+++ src/app/system.ts
@@ -0,0 +1,17 @@
+import { App, Server } from 'corpjs-express'
+import Config, { loaders, processors } from 'corpjs-config'
+import Endpoints from 'corpjs-endpoints'
+import Router from './Router'
+import System from 'corpjs-system'
+import Logger from 'corpjs-logger'
+const {name} = require('../../package.json')
+
+export default new System({name})
+    .add('config', new Config()
+        .add(config => loaders.require({ path: './config/default.js', mandatory: true })))
+    .add('logger', Logger()).dependsOn({ component: 'config', source: 'logger', as: 'config' })
+    .add('endpoints', Endpoints()).dependsOn({ component: 'config', source: 'endpoints', as: 'config' })
+    .add('app', App())
+    .add('router', Router()).dependsOn('app', 'endpoints', 'logger')
+    .add('server', Server()).dependsOn('app', 'router', { component: 'config', source: 'server', as: 'config' })
+    .logAllEvents()
